2abc136c32e00bf6d1217b6a29c1347d99b3eff0
Alexandr Baranov <alexbrnov@gmail.com>
2023-09-29 17:57:14 +0300

    IB Connections

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorDisplayView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func redSliderAction() {
+    }
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
 }
 
